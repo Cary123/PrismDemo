e8d3e6d1c7370b8dd9c12afb6eeb6f4f9c7bf94b
GaoFeng <2191936379@qq.com>
2017-09-24 10:59:27 +0800

    Add project files.

---
+++ PrismDemo/PrismEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Events;
+
+namespace PrismDemo
+{
+    public class OpenFormEvent :PubSubEvent
+    {
+    }
+    public class CloseFormEvent : PubSubEvent
+    {
+    }
+    public class SynchronizeABEvent : PubSubEvent
+    {
+    }
+    public class ReloadFormEvent : PubSubEvent
+    {
+    }
+}
